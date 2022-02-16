168ac9a0d5b9a7fb1495aea71b52cf94936b6894
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-16 06:34:50 +0000

    routes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,11 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
-  
+
   has_one_attached :product_image
-  
-  
+
+
   def get_product_image(size)
-    product_image.attached? ? pr_image : 'no_image.jpg'
+    product_image.attached? ? product_image : 'no_image.jpg'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :end_users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+root :to => 'homes#top'
+get "homes/about" => "homes#about"
+
+
+resource :end_users, only: [:show, :unsubscribe, :withdraw, :edit, :update]
+resources :products, only: [:index, :show]
+resources :orders, only: [:index, :show, :comfirm, :complete, :new, :create]
+resources :cart_items, only: [:index, :create, :update, :destroy, :destroy_all]
+resources :deliveries, only: [:index, :edit, :update, :create, :destroy]
+
+namespace :admin do
+  resources :products, only: [:index, :new, :create, :show, :edit, :update]
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :end_users, only: [:index, :show, :edit, :update, :withdraw]
+  resources :orders, only: [:index, :show, :update]
+  resources :order_details, only: [:update]
+end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
