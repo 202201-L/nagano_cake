0755626538b63812f9cd5baeaea39514fb169fd2
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-16 07:42:08 +0000

    ジャンル機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/new'
+    get 'genres/edit'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :end_users, controllers: {
