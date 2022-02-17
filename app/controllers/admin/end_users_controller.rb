fa48ff02542e3337466a20cf421636ca10515a7c
EC2 Default User <ec2-user@ip-172-31-93-123.ec2.internal>
2022-02-17 08:45:40 +0000

    test

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -4,9 +4,16 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def edit
+    @end_user = EndUser.find(params[:id])
   end
 
   def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user)
+    else
+      render "edit"
+    end
   end
 
   def withdraw
@@ -14,5 +21,9 @@ class Admin::EndUsersController < ApplicationController
 
   def show
     @end_user = EndUser.find(params[:id])
+  end
+
+  def end_user_params
+    params.require(:end_user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code,:address)
   end
 end
