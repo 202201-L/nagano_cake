062e50ee8826acc9c4352011f63824e61c05e0f3
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-02-17 03:30:10 +0000

    [add]new/edit/show

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,35 @@
 class Admin::ProductsController < ApplicationController
+
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_path(@product.id)
   end
 
   def index
+    if params[genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @products = @genre.products
+    else
+      @products = Product.all
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :body, :non_tax_price, :sale_status, :image)
+  end
+
 end
