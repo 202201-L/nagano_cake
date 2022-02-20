520c2cb71e6a3dd5d325a5c1e4d96d937180174d
EC2 Default User <ec2-user@ip-172-31-93-123.ec2.internal>
2022-02-20 11:15:25 +0000

    test

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,19 +9,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-  cart_items = current_customer.cart_items.all
+  cart_items = current_end_user.cart_items.all
 
   @order = current_end_user.orders.new(order_params)
   if @order.save
     cart_items.each do |cart|
       order_detail = OrderDetail.new
-      order_detail.product_id = cart.item_id
-      order_product.order_id = @order.id
-      order_detail.order_amount = cart.amount
-      order_detail.order_price = cart.item.price
+      order_detail.product_id = cart.product_id
+      order_detail.order_id = @order.id
+      order_detail.amount = cart.amount
+      order_detail.intax_price = cart.product.in_tax_price
       order_detail.save
     end
-    redirect_to orders_complete
+    redirect_to orders_complete_path
     cart_items.destroy_all
   else
     render :new
@@ -34,6 +34,20 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.postage = 800
     @total = @cart_items.inject(0) { |sum, item| sum + item.sub_total }
+
+    if delivery_params[:address_number] == "1"
+      @order.post_code = current_end_user.post_code
+      @order.address = current_end_user.address
+      @order.name = current_end_user.last_name + current_end_user.first_name
+
+    elsif delivery_params[:address_number] == "2"
+      delivery = Delivery.find(delivery_params[:delivery_id])
+      @order.post_code = delivery.post_code
+      @order.address = delivery.address
+      @order.name = delivery.address_name
+    end
+
+
 
   end
 
@@ -51,11 +65,11 @@ class Public::OrdersController < ApplicationController
 
   private
     def   order_params
-    params.require(:order).permit(:name, :address, :total_price, :postage, :payment_method)
+    params.require(:order).permit(:name, :address, :total_price, :postage, :payment_method,:post_code)
     end
 
     def delivery_params
-    params.require(:order).permit(:name, :address)
+    params.require(:order).permit(:name, :address,:post_code,:address_number,:delivery_id)
     end
 
 
