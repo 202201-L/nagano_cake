4f5e508e593c591e1c3aa19afd3b6e1a3b5adc09
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-21 09:28:12 +0000

    finish

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,13 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    if @order.order_details.find_by(production_status: 2).present?
+      @order.update(order_status: 2)
+    end
+    if @order.order_details.where(production_status: 3).count == @order.order_details.count
+      @order.update(order_status: 3)
+    end
     redirect_back(fallback_location: root_path)
   end
 
