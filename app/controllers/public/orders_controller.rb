2adb8dbbcca60d492f079aaff33798a863db927a
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-02-22 02:05:34 +0000

    [add]layout

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,26 +1,26 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
   end
-  
+
   def create
-    
+
   end
-  
+
   def index
-  
+
   end
-  
+
   def show
-  
+
   end
-  
+
   def confirm
-    
+
   end
-  
+
   def complete
-    
+
   end
 end
